c9a1073f9cbf8450b0bbe3b0369d499330326ee2
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-05-27 22:43:27 +0100

    update consumers.py

--- chatsocket/consumers.py
+++ chatsocket/consumers.py
@@ -11,14 +11,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
         
     async def disconnect(self, close_code):
-        await.self.channel_layer.group_discard(
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
         
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data["message"]
-        await.self.channel_layer.group_send(
+        await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat.message", "message": message}
         )
         
