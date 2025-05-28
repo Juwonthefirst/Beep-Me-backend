a41f76f91789aac8be34119054d2cfa39fcc5549
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-05-28 09:03:50 +0100

    update settings.py

--- BeepMe/settings.py
+++ BeepMe/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = [os.getenv("hostname")]
+ALLOWED_HOSTS = [os.getenv("HOST_NAME")]
 
 
 # Application definition
