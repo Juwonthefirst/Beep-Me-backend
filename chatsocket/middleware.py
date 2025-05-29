b0808ec92a909d4ca7c2ec8cf948a3dbc645512c
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-05-29 10:31:40 +0100

    update middleware.py

--- chatsocket/middleware.py
+++ chatsocket/middleware.py
@@ -1,12 +1,13 @@
-from channels.middleware import BaseMiddleware
-from channels.db import database_sync_to_async
-from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import AccessToken
-from urllib.parse import parse_qs
-
-
-class JWTAuthMiddleware(BaseMiddleware):
-	async def __call__(self, scope, recieve, send):
+class JWTAuthMiddleware:
+	def __init__(self, app):
+		self.app = app
+	async def __call__(self, scope, receive, send):
+		
+		from django.contrib.auth import get_user_model
+		from rest_framework_simplejwt.tokens import AccessToken
+		from channels.db import database_sync_to_async
+		from urllib.parse import parse_qs
+		
 		query_string = scope["query_string"].decode()
 		token = parse_qs(query_string).get("token", [None])[0]
 		scope["user"] = None
@@ -19,4 +20,4 @@ class JWTAuthMiddleware(BaseMiddleware):
 			except:
 				scope["user"] = None
 		
-		return await super().__call__(scope, recieave, send)
+		return await self.app(scope, receive, send)
