d5566036c78b1b6354b04b5ed309ed8d1424fcae
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-05-30 16:18:40 +0100

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
 	path("", include("dj_rest_auth.urls")),
     path("registration/", include("dj_rest_auth.registration.urls")),
-    path("social/google/", views.GoogleLogin)
+    path("social/google/", views.googleLogin)
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,33 +1,28 @@
 from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
-from channels.db import database_sync_to_async
 from secrets import token_hex
-from asgiref.sync import sync_to_async
 from google.oauth2 import id_token
 from google.auth.transport import requests
 
 User = get_user_model()
 bad_request = status.HTTP_400_BAD_REQUEST
 
-@sync_to_async
 def verify_id_token(token):
 	try:
 		return id_token.verify_oauth2_token(token, requests.Request())
 	except:
 		return None
-		
-async def GoogleLogin(request):
-	if request.method != "POST": 
-		return Response({"error": "This method is not allowed"}, status = bad_request)
-		
+
+@api_view(["POST"])		
+def googleLogin(request):
 	id_token = request.data.get("token")
 	if not id_token: 
 		return Response({"error": "token is required"}, status = bad_request)
 			
-	data = await verify_id_token(id_token)	
+	data = verify_id_token(id_token)	
 	if not data: 
 		return Response({"error": "Invalid token"}, status = bad_request)
 			
@@ -36,10 +31,10 @@ async def GoogleLogin(request):
 		return Response({"error": "Unable to get email"}, status = bad_request)
 				
 	try: 
-		user = await database_sync_to_async(User.objects.get)(email = email)
+		user = User.objects.get(email = email)
 		new_user = False
 	except User.DoesNotExist:
-		user = await database_sync_to_async(User.objects.create_user)(username = email.replace('@', "_"), email = email, password = f'pass_{token_hex(32)}')
+		user = User.objects.create_user(username = email.replace('@', "_"), email = email, password = f'pass_{token_hex(32)}')
 		new_user = True
 				
 	refresh_token = RefreshToken.for_user(user)
