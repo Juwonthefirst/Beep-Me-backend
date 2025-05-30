29cca399e22bc3f1c5f82973ff03d230e91ddf5c
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-05-30 15:34:47 +0100

    update

--- authentication/views.py
+++ authentication/views.py
@@ -18,38 +18,39 @@ def verify_id_token(token):
 		return id_token.verify_oauth2_token(token, requests.Request())
 	except:
 		return None
-
-class GoogleLoginView(APIView):
-	async def post(self, request):
-		id_token = await request.data.get("token")
-		if not id_token: 
-			return Response({"error": "token is required"}, status = bad_request)
-			
-		data = await verify_id_token(id_token)	
-		if not data: 
-			return Response({"error": "Invalid token"}, status = bad_request)
-			
-		email = data.get("email")
-		if not email:
-			return Response({"error": "Unable to get email"}, status = bad_request)
-				
-		try: 
-			user = await database_sync_to_async(User.objects.get)(email = email)
-			new_user = False
-		except User.DoesNotExist:
-			user = await database_sync_to_async(User.objects.create_user)(username = email.replace('@', "_"), email = email, password = f'pass_{token_hex(32)}')
-			new_user = True
-				
-		refresh_token = RefreshToken.for_user(user)
-			
-		return Response({
-			"refresh": str(refresh_token),
-			"access": str(refresh_token.access_token),
-			"user": {
-				"id": user.id,
-				"username": user.username,
-				"email": user.email,
-			},
-			"new_user": new_user
-		})
 		
+async def GoogleLogin(request):
+	if request.method != "POST": 
+		return Response({"error": "This method is not allowed"}, status = bad_request)
+		
+	id_token = await request.data.get("token")
+	if not id_token: 
+		return Response({"error": "token is required"}, status = bad_request)
+			
+	data = await verify_id_token(id_token)	
+	if not data: 
+		return Response({"error": "Invalid token"}, status = bad_request)
+			
+	email = data.get("email")
+	if not email:
+		return Response({"error": "Unable to get email"}, status = bad_request)
+				
+	try: 
+		user = await database_sync_to_async(User.objects.get)(email = email)
+		new_user = False
+	except User.DoesNotExist:
+		user = await database_sync_to_async(User.objects.create_user)(username = email.replace('@', "_"), email = email, password = f'pass_{token_hex(32)}')
+		new_user = True
+				
+	refresh_token = RefreshToken.for_user(user)
+			
+	return Response({
+		"refresh": str(refresh_token),
+		"access": str(refresh_token.access_token),
+		"user": {
+			"id": user.id,
+			"username": user.username,
+			"email": user.email,
+		},
+		"new_user": new_user
+	})
