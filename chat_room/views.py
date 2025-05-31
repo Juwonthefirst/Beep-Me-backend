cc761005b3dd2723639ec049b4e0364da471ce12
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-05-31 16:02:31 +0100

    update views.py

--- chat_room/views.py
+++ chat_room/views.py
@@ -2,19 +2,20 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from .serializers import RoomMessagesSerializer
 from .models import ChatRoom
 
 class RoomMessagesView(ListAPIView):
 	serializer_class = RoomMessagesSerializer
-	permission_classes = [IsAuthenticated, IsMember]
+	permission_classes = [IsAuthenticated]
 	def get_queryset(self): 
 		room_id = self.kwargs["room_id"]
 		try: 
 			#checks if the user is a member of the room before returning messages
 			room = ChatRoom.objects.get(id = room_id)
 			if not room.members.filter(id = self.request.user.id).exists(): 
-				raise ChatRoom.DoesNotExist
+				raise PermissionDenied
 				
 			return room.messages.all().order_by("timestamp")
 		except ChatRoom.DoesNotExist: 
