7f2b6fb3004025524b29153b6e1664769072e0fb
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-05-31 17:05:45 +0100

    update models.py

--- user/models.py
+++ user/models.py
@@ -11,6 +11,6 @@ username_validator = RegexValidator(
 
 
 class CustomUser(AbstractUser): 
-	username = CharField(max_length = 60, unique = True, validators = [username_validator], error_messages = {"unique": "a user with this username already exists"})
+	username = models.CharField(max_length = 60, unique = True, validators = [username_validator], error_messages = {"unique": "a user with this username already exists"})
 	email = models.EmailField(unique = True)
 	friends = models.ManyToManyField("self", symmetrical = True)
