c723b54ac3146d11c32fc7d993d619836c81cd7e
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-01 13:43:04 +0100

    update urls.py

--- chat_room/urls.py
+++ chat_room/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("<int:room_id>/", views.RoomDetailsView.as_view()),
+	path("<int:pk>/", views.RoomDetailsView.as_view()),
 	path("<int:room_id>/messages/", views.RoomMessagesView.as_view()),
 	path("<int:room_id>/members/", views.RoomMembersView.as_view()),
 ]
