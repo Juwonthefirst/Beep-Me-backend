84325834f3a02579f4c93b8a444c8aaf8ed7c9f8
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-04 14:40:18 +0100

    update serializers.py

--- group/serializers.py
+++ group/serializers.py
@@ -2,18 +2,9 @@ from rest_framework import serializers
 from .models import Group, MemberDetails
 from chat_room.models import ChatRoom
 
-class GroupMemberSerializer(serializers.ModelSerializer): 
-   member_id = serializers.ReadOnlyField(source = "member.id")
-   member_username = serializers.ReadOnlyField(source = "member.username")
-   
-   class Meta: 
-       model = MemberDetails
-       fields = ["role", "joined_at", "member_id", "member_username"]
-       extra_kwargs = {
-           "joined_at": {
-               "read_only": True
-           }
-       }
+class MembersSerializer(serializers.Serializer): 
+   member_id = serializers.IntegerField()
+   role = serializers.CharField(max_length = 100)
        
 class GroupSerializer(serializers.ModelSerializer): 
 	members = GroupMemberSerializer(many = True)
