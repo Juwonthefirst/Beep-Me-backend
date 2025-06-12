9828ada48855a2e9c1bb140ae2d731e3aadf5c8d
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-12 13:15:45 +0100

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
 	path("", include("dj_rest_auth.urls")),
     path("registration/", include("dj_rest_auth.registration.urls")),
-    path("social/google/", views.googleLogin)
+    path("social/google/ID", views.googleLoginByIdToken),
+    path("social/google/code", views.googleLoginByCode),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -7,8 +7,13 @@ from secrets import token_hex
 from google.oauth2 import id_token
 from google.auth.transport import requests
 from allauth.account.models import EmailAddress
+from authlib.integrations.requests_client import OAuth2Session
+
 User = get_user_model()
 bad_request = status.HTTP_400_BAD_REQUEST
+
+google_client_id = os.getenv("GOOGLE_CLIENT_ID")
+google_client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
 
 def verify_id_token(token):
 	try:
@@ -17,7 +22,7 @@ def verify_id_token(token):
 		return None
 
 @api_view(["POST"])		
-def googleLogin(request):
+def googleLoginByIdToken(request):
 	id_token = request.data.get("token")
 	if not id_token: 
 		return Response({"error": "token is required"}, status = bad_request)
@@ -47,15 +52,66 @@ def googleLogin(request):
 				
 	refresh_token = RefreshToken.for_user(user)
 			
-	return Response({
-		"refresh": str(refresh_token),
+	response = Response({
 		"access": str(refresh_token.access_token),
-		"user": {
-			"id": user.id,
-			"username": user.username,
-			"email": user.email,
-			"firstname": user.first_name,
-			"lastname": user.last_name
-		},
 		"new_user": new_user
 	})
+	
+	response.set_cookie(
+		key = "refresh_token",
+		value = str(refresh_token),
+		secure = True,
+		httponly = True,
+		max_age = 60 * 60 * 24 * 30,
+		samesite = 'None'
+	)
+	
+	return response
+
+@api_view(["POST"])	
+def googleLoginByCode(request): 
+	code = request.data.get("code")
+	if not code: 
+		return Response({"error": "No code provided"}, status = bad_request)
+		
+	client = OAuth2Session(google_client_id, google_client_secret, redirect_uri = "postmessage")
+	token = client.fetch_token("https://oauth2.googleapis.com/token", code = code)
+	id_token = token.get("id_token")
+	id_info = client.parse_id_token(id_token)
+	email = id_info.get("email")
+	
+	if not email:
+		return Response({"error": "Unable to get email"}, status = bad_request)
+				
+	try: 
+		user = User.objects.get(email = email)
+		if not user.is_active: 
+			raise User.DoesNotExist
+		new_user = False
+	except User.DoesNotExist:
+		user = User.objects.create_user(username = email.rstrip("@gmail.com") + str(token_hex(8)), email = email, password = f'pass_{token_hex(32)}')
+		EmailAddress.objects.create(
+			user = user,
+			email = email,
+			primary = True,
+			verified = True
+		)
+		new_user = True
+				
+	refresh_token = RefreshToken.for_user(user)
+			
+	response = Response({
+		"access": str(refresh_token.access_token),
+		"new_user": new_user
+	})
+	
+	response.set_cookie(
+		key = "refresh_token",
+		value = str(refresh_token),
+		secure = True,
+		httponly = True,
+		max_age = 60 * 60 * 24 * 30,
+		samesite = 'None'
+	)
+	
+	return response
