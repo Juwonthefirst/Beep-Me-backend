0775b23086ef72a9ae8c47c0c9516b59043b3326
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-13 10:11:36 +0100

    update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	path("", include("dj_rest_auth.urls")),
 	path("login/", views.CustomLoginView.as_view()),
-	path("logout/", views.logoutView)
+	path("logout/", views.logoutView),
     path("registration/", include("dj_rest_auth.registration.urls")),
     path("social/google/ID/", views.googleLoginByIdToken),
     path("social/google/code/", views.googleLoginByCode),
