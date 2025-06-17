1798b4d188cfe0dc0d46557e0dda343391d0b1a6
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-17 20:08:08 +0100

    update serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -12,4 +12,9 @@ class RetrieveUsersSerializer(serializers.ModelSerializer):
 	class Meta: 
 		model = User
 		fields = "__all__"
+		extra_kwargs = {
+		    "password": {
+		        "write_only": True
+		    }
+		}
 		
