df2d4ba0ff78d949016ac12a4b66c997b4b75e71
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-21 01:52:57 +0100

    update adapters.py

--- authentication/adapters.py
+++ authentication/adapters.py
@@ -5,5 +5,5 @@ class EmailAdapter(DefaultAccountAdapter):
 	def send_mail(self, template_prefix, email, context): 
 		if "activate_url" in context: 
 			key = context["key"]
-			context["activate_url"] = f"{os.getenv('FRONTEND_HOST_NAME')}/verify-email/?key={key}"
+			context["activate_url"] = f"{os.getenv('FRONTEND_HOST_NAME')}/verify-email?key={key}"
 		return super().send_mail(template_prefix, email, context)
