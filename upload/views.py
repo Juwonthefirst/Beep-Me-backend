ed4398b191a75d106e4a52c1c2e2180dc6e43938
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-23 20:51:13 +0100

    update views.py

--- upload/views.py
+++ upload/views.py
@@ -1,4 +1,4 @@
-from rest_framework.parser import MultiPartParser
+from rest_framework.parsers import MultiPartParser
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
