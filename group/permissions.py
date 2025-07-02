5f7398c3a537efc599969fa978faf8e7dbe47723
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-07-02 08:20:34 +0100

    update permissions.py

--- group/permissions.py
+++ group/permissions.py
@@ -9,20 +9,12 @@ class IsAdminOrReadOnly(BasePermission):
 		return (obj.role == "admin") or (request.method in SAFE_METHODS)
 		
 		
-class CanRemoveUser(BasePermission): 
-	def has_object_permission(self, request, view, obj):
-		group_id = request.kwargs.get("pk")
-		member = MemberDetail.objects.filter(group_id = group_id, member = request.user)
-		return member.role.permissions.filter(action = "can remove user").exists()
-		
-class CanAddUser(BasePermission): 
-	def has_object_permission(self, request, view, obj):
-		group_id = request.kwargs.get("pk")
-		member = MemberDetail.objects.filter(group_id = group_id, member = request.user)
-		return member.role.permissions.filter(action = "can add user").exists()
-		
-class CanChangeUserRole(BasePermission): 
-	def has_object_permission(self, request, view, obj):
-		group_id = request.kwargs.get("pk")
-		member = MemberDetail.objects.filter(group_id = group_id, member = request.user)
-		return member.role.permissions.filter(action = "can change role").exists()
+def has_group_permission(action): 
+	
+	class BaseGroupPermission(BasePermission): 
+		def has_object_permission(self, request, view, obj):
+			group_id = request.kwargs.get("pk")
+			member = MemberDetail.objects.filter(group_id = group_id, member = request.user)
+			return member.role.permissions.filter(action = action).exists()
+			
+	return BaseGroupPermission
