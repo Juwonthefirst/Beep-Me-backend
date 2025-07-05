8431052027281f1eb0cbbc418b7a5755059ed323
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-07-05 08:27:48 +0100

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-
+from user import views as user_views
 
 urlpatterns = [
 	path("login/", views.CustomLoginView.as_view()),
@@ -10,5 +10,11 @@ urlpatterns = [
     path("social/google/code/", views.google_login_by_code_token),
 	path("token/refresh/", views.CustomTokenRefreshView.as_view()),
 	path("user/csrf/", views.get_csrf),
+	path("user/rooms/", user_views.UserChatRoomsView.as_view()),
+	path("user/notifications/", user_views.UserNotificationsView.as_view()),
+	path("user/friends/", user_views.FriendListView.as_view()),
+	path("user/friend-requests/send/", user_views.SentFriendRequestView.as_view()),
+	path("user/friend-requests/send/", user_views.sendFriendRequest),
+	path("user/friend-requests/receive/", user_views.receivedFriendRequestView.as_view()),
 	path("", include("dj_rest_auth.urls")),
 ]
--- user/urls.py
+++ user/urls.py
@@ -6,12 +6,4 @@ urlpatterns = [
 	path("", views.UsersView.as_view()),
 	path("<int:pk>/", views.RetrieveUserView.as_view()),
 	path("exists/", views.DoesUsernameExistView.as_view()),
-	path("rooms/", views.UserChatRoomsView.as_view()),
-	path("notifications/", views.UserNotificationsView.as_view()),
-	path("friends/", views.FriendListView.as_view()),
-	path("friend-requests/send/", views.SentFriendRequestView.as_view()),
-	path("friend-requests/send/", views.sendFriendRequest),
-	path("friend-requests/receive/", views.receivedFriendRequestView.as_view()),
-
-	
 ]
