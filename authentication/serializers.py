74cdb8feeab43613d2e83d7b34fcdf744a2f45d7
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-07-14 07:17:14 +0100

    replaced required with allow_blank in the login serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 
 class LoginSerializer(serializers.Serializer): 
-	username = serializers.CharField(max_length = 60, required = False)
-	email = serializers.EmailField(required = False)
+	username = serializers.CharField(max_length = 60, allow_blank = True)
+	email = serializers.EmailField(allow_blank= True)
 	password = serializers.CharField(write_only = True)
 	
 	def validate(self, attrs):
