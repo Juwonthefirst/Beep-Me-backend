09b7df48d6487a1f542674f0232db2ef47b4db59
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-07-16 18:28:57 +0100

    update consumers.py

--- chatsocket/consumers.py
+++ chatsocket/consumers.py
@@ -112,7 +112,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             case "chat": 
                 timestamp = timezone.now().isoformat()
                 room = self.currentRoom
-                if room and room.name != room_name:
+                if room.name != room_name:
                     return 
                 await self.channel_layer.group_send(
                     room_name, {
